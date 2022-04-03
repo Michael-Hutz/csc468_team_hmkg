f7735faeb76eb0b6be803f40b4a12f0118b45cc7
Michael-Hutz <71849048+Michael-Hutz@users.noreply.github.com>
2022-04-03 19:22:58 -0400

    Add files via upload

--- rngtech/rngtech.py
+++ rngtech/rngtech.py
@@ -1,1 +1,62 @@
+from flask import Flask, Response
+import os
+import socket
+import time
+import random
 
+##app = Flask(_name_)
+
+historychange = []
+historyvalue = []
+direction = 0
+weight = 0
+val = 300.30
+
+##This will turn into a loop for running if we are just going to display a stream.
+def rngetf(val):
+    
+    
+    i = random.randint(1,8)
+
+##   if direction != 0:
+##           if direction == 1:
+##               i += 1
+       
+##           else:
+##                i += -1
+
+    if i != 0 and i <= 4:
+        direction = -1
+
+    if i != 0 and i > 4:
+        direction = 1 
+
+    if direction == -1:
+            ##IF making a livestream, these numbers will have to be reduced down
+
+            ##Crash Flag
+            
+            weight = random.uniform(-.01, -0.0015)
+
+            weight = val * weight
+            historychange.append(weight)
+            val = val + weight
+
+    if direction == 1:
+            ##IF making a livestream, these numbers will have to be reduced down
+
+            ##Rocket flag
+
+            weight = random.uniform(.01, .00399)
+
+            weight = val * weight
+            historychange.append(weight)
+            val = val + weight
+            
+    return val
+
+i = 0
+while i < 20:
+    print(rngetf(val))
+    print(historychange[i])
+    i += 1
